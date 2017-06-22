545aad4a5bff47c633c134c41f446cd1239e1d8f
Marco Zocca <marco.zocca@recordunion.com>
2017-06-22 19:27:14 +0200

    added attoparsec

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,1 +1,8 @@
 module Lib where
+
+import Data.Attoparsec.Internal.Types (Parser)
+import qualified Data.Attoparsec.ByteString as PB
+import Data.Attoparsec.ByteString.Char8 (decimal, signed, scientific, digit, number, rational, char, char8, endOfLine, endOfInput, isDigit, isDigit_w8, isEndOfLine, isHorizontalSpace)
+import qualified Data.ByteString as B (ByteString, readFile)
+
+import System.FilePath
